0fb5c2fd37254984dd3e8cf7b321a8fd98405df3
Michael Kang <43582949+michaelkang84@users.noreply.github.com>
2026-05-12 15:33:57 -0400

    basic data types

--- lesson1.py
+++ lesson1.py
@@ -1,2 +1,20 @@
 print("Hello, World!")
 print("Another push")
+
+example_var = 5
+
+# reassignment
+example_var = 8
+
+print(example_var)
+
+# basic data types
+float_1 = 3.4
+integer_1 = 10
+bool_1 = True
+
+float_1 = 3.3
+
+print(float_1)
+print(integer_1)
+print(bool_1)
