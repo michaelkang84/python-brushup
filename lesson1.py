8b6cb59fb2e788fd8aa9a0908857ca67a9f9c4d8
Michael Kang <43582949+michaelkang84@users.noreply.github.com>
2026-05-12 16:02:11 -0400

    print and floats

--- lesson1.py
+++ lesson1.py
@@ -18,3 +18,31 @@ float_1 = 3.3
 print(float_1)
 print(integer_1)
 print(bool_1)
+
+# math operators
+addition = 4 + 5
+subtraction = 4 - 5
+multiplication = 4 * 5
+division = 4 / 5
+print(division)
+
+
+exponentiation = 4 ** 5
+print(exponentiation)
+# divides left by right
+# returns integer that left of any decimal
+floor_division = 4 // 5
+print(floor_division)
+# remainder after dividing left by right
+modulo_division = 4 % 90
+print(modulo_division)
+
+# assignment operator
+add_assign = 5
+add_assign += 4
+print(add_assign)
+
+# 4.029999999999999 no 4.03
+# written on top C cannot compute correctly
+print(1.23 + 2.80)
+
